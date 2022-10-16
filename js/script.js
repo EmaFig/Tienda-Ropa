6cd27f0f0325d03c8276732da8ce81bf50070f42
manu figueroa <manuchat59@hotmail.com>
2022-10-16 16:48:44 -0300

    Index Finalizado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 /* SCRIPTS */
+
+/*INDEX*/
+function mostrar_boton_1(){
+    document.getElementById("carrito_oferta_1").style.visibility="visible";
+}
+
+function ocultar_boton_1(){
+    document.getElementById("carrito_oferta_1").style.visibility="hidden";
+}
+
+function mostrar_boton_2(){
+    document.getElementById("carrito_oferta_2").style.visibility="visible";
+}
+
+function ocultar_boton_2(){
+    document.getElementById("carrito_oferta_2").style.visibility="hidden";
+}
+
+function mostrar_boton_3(){
+    document.getElementById("carrito_oferta_3").style.visibility="visible";
+}
+
+function ocultar_boton_3(){
+    document.getElementById("carrito_oferta_3").style.visibility="hidden";
+}
+
+
+function mostrar_boton_4(){
+    document.getElementById("carrito_oferta_4").style.visibility="visible";
+}
+
+function ocultar_boton_4(){
+    document.getElementById("carrito_oferta_4").style.visibility="hidden";
+}
+
