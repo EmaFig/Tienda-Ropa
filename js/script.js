3fd8d768609ef77210cc5b4fb1239d09557aac7b
manu figueroa <manuchat59@hotmail.com>
2022-10-25 19:05:39 -0300

    COntacto y Quienes Somos

--- js/script.js
+++ js/script.js
@@ -34,3 +34,23 @@ function ocultar_boton_4(){
     document.getElementById("carrito_oferta_4").style.visibility="hidden";
 }
 
+/* VALIDAR CONTACTO */
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var correo = document.getElementById('correo').value;
+    if(correo.length == 0) {
+      alert('No has escrito nada en el Correo');
+      return;
+    }
+    var comentario = document.getElementById('comentario').value;
+    if (comentario.length == 0) {
+      alert('No a escrito comentario');
+      return;
+    }
+    this.submit();
+  }
